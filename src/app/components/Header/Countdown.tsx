4fa8bdc951f9e2145aa5fb0593c9bfe935057841
Adeola Akinwole <ajunior488@gmail.com>
2023-07-23 15:55:53 +0100

    complete top-section

---
+++ src/app/components/Header/Countdown.tsx
@@ -0,0 +1,58 @@
+import { siteMetadata } from "@/data/siteMetadata";
+import { daysUntil, getEndOfDayTime } from "@/utils/utils"; // Assuming you have a utility function for getting the end of the day time
+import React, { useEffect, useMemo, useRef, useState } from "react";
+
+const CountdownComponent: React.FC = () => {
+  const memoizedWeddingDate = useMemo(
+    () => new Date(siteMetadata.couplesDetails.weddingDate),
+    [] // Empty dependency array, so it's only initialized once
+  );
+
+  const [counter, setCounter] = useState<number>(
+    daysUntil(memoizedWeddingDate)
+  );
+  const counterElementRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const updateCounterAtMidnight = () => {
+      const now = new Date();
+      const endOfDay = getEndOfDayTime(now); // Get the end of the day time for the current date
+      const timeDifference = endOfDay.getTime() - now.getTime();
+
+      // Update the counter at midnight (end of the day)
+      if (timeDifference <= 1000) {
+        // Check if the time difference is less than or equal to 1 second
+        const newDays = daysUntil(memoizedWeddingDate);
+        setCounter(newDays);
+      }
+    };
+
+    // Set up an interval to check if it's midnight and update the counter accordingly
+    const interval = setInterval(updateCounterAtMidnight, 1000);
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, [memoizedWeddingDate]);
+
+  // Update the CSS custom property '--value' when counter changes
+  useEffect(() => {
+    if (counterElementRef.current) {
+      counterElementRef.current.style.setProperty(
+        "--value",
+        counter.toString()
+      );
+    }
+  }, [counter]);
+
+  return (
+    <span className="countdown font-mono text-6xl">
+      <span
+        ref={counterElementRef}
+        style={{
+          ...{ "--value": counter },
+        }}
+      ></span>
+    </span>
+  );
+};
+export default CountdownComponent;
--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -4,16 +4,17 @@ import { HeaderNavLinks, headerNavLinks } from "@/data/headerNavLinks";
 
 import { siteMetadata } from "@/data/siteMetadata";
 
-import { classNames, getFirstNames } from "@/utils/utils";
+import { classNames, getFirstNames, hasDateElapsed } from "@/utils/utils";
 
 import { motion } from "framer-motion";
 
 import { Bars } from "react-loader-spinner";
 
+import { useEffect, useState } from "react";
 import Tilt from "react-parallax-tilt";
-
 import ParticlesComponent from "../Particles";
-
+import CountdownComponent from "./Countdown";
+import "./style.css";
 const Header = () => {
   return (
     <div className="navbar relative z-[200] p-10 w-full">
@@ -146,7 +147,7 @@ const Name = () => {
                                     rotateY(7.48deg) scale3d(1, 1, 1)`,
                 }}
               >
-                <div className="lg:text-6xl lg:mt-40 text-5xl flex justify-center items-center inset-0 font-primary w-full  h-[40px]">
+                <div className="lg:text-6xl shadow-sm lg:mt-40 text-5xl flex justify-center items-center inset-0 font-primary w-full  h-[40px] flex-wrap">
                   <p className="text-[#f7f1e2]">{coupleNames.hisFirstName}</p>
 
                   <p className="text-[#e15f35]">&nbsp;&&nbsp;</p>
@@ -163,6 +164,13 @@ const Name = () => {
 };
 
 const CircleSvg = () => {
+  const [hasDateElapsedValue, setHasDateElapsedValue] = useState(false);
+
+  useEffect(() => {
+    const value = hasDateElapsed(siteMetadata.couplesDetails.weddingDate);
+    setHasDateElapsedValue(value);
+  }, []);
+
   const containerVariants = {
     initial: {
       rotate: 0,
@@ -258,25 +266,65 @@ const CircleSvg = () => {
           <g>
             <use href="#circlePath" fill="none"></use>
 
-            <text fill="#000">
-              <textPath href="#circlePath">
-                We did it - We got married ♡ Praise Jesus.
-              </textPath>
+            <text
+              style={{
+                fontFamily: "Helvetica Neue,Arial",
+                fontSize: "18.7px",
+                fontWeight: 700,
+                fill: "#eea187",
+                transition: "fill .3 ease",
+                transform: "scale(.777)",
+                transformOrigin: "center",
+              }}
+              fill="#000"
+            >
+              {hasDateElapsedValue ? (
+                <textPath href="#circlePath">
+                  We did it - We got married ♡ Praise Jesus.
+                </textPath>
+              ) : (
+                <textPath href="#circlePath">You are invited.</textPath>
+              )}
             </text>
           </g>
         </motion.svg>
       </div>
 
       <div className="countdown-wrapper">
-        <div
-          className="spinning-smiley is-inview"
-          data-scroll="true"
-          data-scroll-speed="2"
-          style={{
-            transform:
-              "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)",
-          }}
-        ></div>
+        {hasDateElapsedValue ? (
+          <div
+            className="spinning-smiley is-inview"
+            data-scroll="true"
+            data-scroll-speed="2"
+            style={{
+              transform:
+                "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 53.2103, 0, 1)",
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              version="1.1"
+              preserveAspectRatio="xMidYMid meet"
+              x="0"
+              y="0"
+              viewBox="0 0 512 512"
+              xmlSpace="preserve"
+            >
+              <g>
+                <path
+                  xmlns="http://www.w3.org/2000/svg"
+                  d="m256 512c-68.38 0-132.667-26.629-181.02-74.98-48.351-48.353-74.98-112.64-74.98-181.02s26.629-132.667 74.98-181.02c48.353-48.351 112.64-74.98 181.02-74.98s132.667 26.629 181.02 74.98c48.351 48.353 74.98 112.64 74.98 181.02s-26.629 132.667-74.98 181.02c-48.353 48.351-112.64 74.98-181.02 74.98zm0-472c-119.103 0-216 96.897-216 216s96.897 216 216 216 216-96.897 216-216-96.897-216-216-216zm93.737 260.188c-9.319-5.931-21.681-3.184-27.61 6.136-.247.387-25.137 38.737-67.127 38.737s-66.88-38.35-67.127-38.737c-5.93-9.319-18.291-12.066-27.61-6.136s-12.066 18.292-6.136 27.61c1.488 2.338 37.172 57.263 100.873 57.263s99.385-54.924 100.873-57.263c5.93-9.319 3.183-21.68-6.136-27.61zm-181.737-135.188c13.807 0 25 11.193 25 25s-11.193 25-25 25-25-11.193-25-25 11.193-25 25-25zm150 25c0 13.807 11.193 25 25 25s25-11.193 25-25-11.193-25-25-25-25 11.193-25 25z"
+                  fill="#e29e84"
+                ></path>
+              </g>
+            </svg>
+          </div>
+        ) : (
+          <div className="countdown-col-element">
+            <CountdownComponent />
+          </div>
+        )}
       </div>
     </div>
   );
