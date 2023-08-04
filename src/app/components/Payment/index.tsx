4e8236fd29809e896b7e26585fc14aec5884d796
Adeola Akinwole <ajunior488@gmail.com>
2023-08-04 23:21:54 +0100

    uh

--- src/app/components/Payment/index.tsx
+++ src/app/components/Payment/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import { siteMetadata } from "@/data/siteMetadata";
 import Link from "next/link";
 import { useState } from "react";
