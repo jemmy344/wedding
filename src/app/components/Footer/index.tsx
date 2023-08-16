987a1a1127c53afa20367e1cf930960758b7020d
aakinwole <120341598+aakinwole@users.noreply.github.com>
2023-08-17 00:38:59 +0100

    new

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -6,11 +6,11 @@ const Footer = () => {
     <footer className="footer footer-center p-8 text-base-content w-full">
       <div className="flex justify-center item-center w-full font-marhey
        text-secondary text-1xl">
-        Made with <span><HeartSvg /></span> by <a href="https://www.google.com/search?q=Jemida&stick=H4sIAAAAAAAA_-NgU1I1qLCwNDG0NDZJS0kyNE5LtEyyMqgwTEq0NE21NE-1MDRNSUo0WsTK5pWam5mSCACpy2kiMgAAAA&hl=en&mat=CXPnlmjqTO0zElcBeenfiEzIAGfXpEyehwKPNzoikBXeURjEGbeopqR5AZfIOMhMuzluKg5uNg7ApwwLqCsGxRsgIyWXbjpouhsf2WjinXVmGvWYVowYAHs49XxBRYsyZeo&authuser=0" target="_blank"
+        Made with <span><HeartSvg /></span> by <a rel="noreferrer" href="https://www.google.com/search?q=Jemida&stick=H4sIAAAAAAAA_-NgU1I1qLCwNDG0NDZJS0kyNE5LtEyyMqgwTEq0NE21NE-1MDRNSUo0WsTK5pWam5mSCACpy2kiMgAAAA&hl=en&mat=CXPnlmjqTO0zElcBeenfiEzIAGfXpEyehwKPNzoikBXeURjEGbeopqR5AZfIOMhMuzluKg5uNg7ApwwLqCsGxRsgIyWXbjpouhsf2WjinXVmGvWYVowYAHs49XxBRYsyZeo&authuser=0" target="_blank"
           className="hover:text-white hover:cursor-pointer hover:underline
           text-rose-gold">Jemida.</a> Don&apos;t forget to
         <a href={`mailto:${siteMetadata.couplesDetails.contactEmail}`}
-          target="_blank" className="hover:text-white hover:cursor-pointer
+          target="_blank" rel="noreferrer" className="hover:text-white hover:cursor-pointer
            hover:underline text-rose-gold">RSVP.</a>
       </div>
     </footer>
